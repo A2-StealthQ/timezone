32f882863a781752206d6607a60edbeed32bf297
A2 <a2@stealthq.com>
2013-11-03 01:42:06 +0200

    added manifest

--- Modulefile
+++ Modulefile
@@ -1,11 +1,11 @@
 name    'aageyev-timezone'
 version '0.1.0'
-source ''
+source 'https://github.com/A2-StealthQ/timezone.git'
 author 'Andrey Ageyev'
 license 'Apache License, Version 2.0'
-summary ''
-description ''
-project_page ''
+summary 'Configure timezone on ubuntu'
+description 'Reconfigure package tzdata on ubuntu.'
+project_page 'https://github.com/A2-StealthQ/timezone'
 
 ## Add dependencies, if any:
 # dependency 'username/name', '>= 1.2.0'
--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,26 @@
 # Requires: see Modulefile
 #
 # Sample Usage:
-#
+# class 
 class timezone {
 
+  package { "tzdata":
+    ensure  => installed,
+  }
+  file {"timezone":
+    path    => "/etc/timezone",
+    ensure  => present,
+    mode    => 644,
+    owner   => root,
+    group   => root,
+    content => template("timezone/timezone.erb"),
+    notify  => Exec["set-timezone"],
+  }
+  
+  exec{"set-timezone":
+    path        => ["/usr/bin", "/usr/sbin", "/bin"],
+    command     => "dpkg-reconfigure -f noninteractive tzdata",
+    require     => File["timezone"],
+    refreshonly => true,
+  }
 }
